7ee321d520cf3e7562b0fde4f4d464408c685c54
Pavlo Berkut <pavlo.berkut@gmail.com>
2023-01-15 14:39:35 +0200

    add query for search

--- src/index.js
+++ src/index.js
@@ -1,18 +1,74 @@
 import Notiflix from 'notiflix';
 import axios from 'axios';
 
+const formRef = document.querySelector('.search-form');
+const galleryRef = document.querySelector('.gallery');
+
 // console.dir(axios);
 
-getImage();
+formRef.addEventListener('submit', onSearch);
 
-function getImage() {
+function onSearch(evt) {
+  evt.preventDefault();
+
+  const query = evt.currentTarget.elements.searchQuery.value;
+  getImage(query);
+}
+
+function getImage(query) {
   const BASE_URL = 'https://pixabay.com/api/';
   const API_KEY = '32766360-76e7eba189222bd8a15da9e43';
-  const apiUrl = `${BASE_URL}?key=${API_KEY}&q=cat&image_type=photo&orientation=horizontal&safesearch=true`;
+  const apiUrl = `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`;
+
   axios
     .get(apiUrl)
     .then(({ data }) => {
-      console.dir(data);
+      markupPhotoCard(data.hits);
     })
     .catch(console.log);
+}
+
+function markupPhotoCard(arr) {
+  const isNUllArr = arr.length < 0;
+  if (isNUllArr) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return;
+  }
+  const markup = arr
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `
+      <div class="photo-card">
+  <img src="${webformatURL}" alt="${comments}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b> ${likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b> ${views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b> ${comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b> ${downloads}
+    </p>
+  </div>
+</div>
+          `;
+      }
+    )
+    .join('');
+
+  galleryRef.innerHTML = markup;
 }
