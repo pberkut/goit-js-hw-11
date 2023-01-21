00c1a7a1036400b1c3cc3f45fec6b6053fc66f17
Pavlo Berkut <pavlo.berkut@gmail.com>
2023-01-21 11:42:36 +0200

    add button to top

--- src/js/refs.js
+++ src/js/refs.js
@@ -4,6 +4,7 @@ const refs = {
   guard: document.querySelector('.guard'),
   spinner: document.querySelector('.js-spinner'),
   backdrop: document.querySelector('.backdrop'),
+  toTopBtn: document.querySelector('.to_top__btn'),
 };
 
 export { refs };
